fe2354bd0b442f3e864cf2f2da85d41f5d3bd301
Gockster <gm.dimitriadis@hotmail.com>
2020-10-18 16:00:13 +0300

    Final touches

--- src/bankaccountapp/BankAccountApp.java
+++ src/bankaccountapp/BankAccountApp.java
@@ -1,30 +1,43 @@
 package bankaccountapp;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class BankAccountApp {
 
 	public static void main(String[] args) {
 		
+		List<Account> accounts = new LinkedList<Account>();
 		
-		Checking chkacc1 = new Checking("Tom Wilson", "32156", 5800);
-		
-		Savings savacc1 = new Savings("Jon Snow","46466", 2500);
-		
-		//savacc1.
-		
-		savacc1.showInfo();
-		System.out.println("*****************************");
-		chkacc1.showInfo();
-		
-		
-		
-		
-//		savacc1.deposit(5000);
-//		savacc1.withdraw(200);
-//		savacc1.transfer("Brokerage", 3000);
-		
+				
 		//Read a csv file then create new accounts based on that data
+		String file = "C:\\Users\\gmdim\\OneDrive\\Υπολογιστής\\MyEclipseWorkspace\\BankAccountApp\\NewBankAccounts.csv";
+		List<String[]> newAccountHolders  = utilities.CSV.read(file);
+		for(String[] accountHolder: newAccountHolders) {
+			String name = accountHolder[0];
+			String sSN = accountHolder[1];
+			String accountType = accountHolder[2];
+			double initDeposit = Double.parseDouble(accountHolder[3]);
+			//System.out.println(name + " " + sSN + " " + accountType + " €" + initDeposit);
+			if(accountType.contentEquals("Savings")) {
+				//System.out.println("OPEN A SAVINGS ACCOUNT");
+				accounts.add(new Savings(name, sSN, initDeposit));
+			} else if (accountType.equals("Checking")) {
+				//System.out.println("OPEN A CHECKING ACCOUNT");
+				accounts.add(new Savings(name, sSN, initDeposit));
+			} else {
+				System.out.println("ERROR READING ACCOUNT TYPE");
+			}
+		} 
 		
-
+		//accounts.get(5).showInfo();
+		
+		for(Account acc: accounts) {
+			System.out.println("\n**************");
+			acc.showInfo();
+		}
+		
+		//accounts.get((int) Math.random() * accounts.size()).deposit(10000);
+		//accounts.get((int) Math.random() * accounts.size()).deposit(1500);
 	}
-
 }
